0ce5ce1eca44d3a26d42f826da10d032f322f9ec
khrystyna.dudko <khrystyna.dudko89@gmail.com>
2023-09-26 10:05:41 +0200

    menubar again

--- client/src/components/Menubar.js
+++ client/src/components/Menubar.js
@@ -1,30 +1,27 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "../styles/Menubar.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Menubar.css';
+
+// Importing SVGs first, instead of linking directly from ../assets in each link, to ensure correct rendering
+import settingsIcon from '../assets/settings.svg';
+import listIcon from '../assets/list.svg';
+import progressIcon from '../assets/progress.svg';
+import homeIcon from '../assets/home.svg';
 
 function MenuBar({ activeScreen }) {
   return (
     <nav className="menu-bar">
-      <Link
-        to="/settings"
-        className={activeScreen === "settings" ? "active" : ""}
-      >
-        <img src="../assets/settings.svg" alt="settings" />
+      <Link to="/settings" className={activeScreen === 'settings' ? 'active' : ''}>
+        <img src={settingsIcon} alt="settings" />
       </Link>
-      <Link
-        to="/edit-habit"
-        className={activeScreen === "edit-habit" ? "active" : ""}
-      >
-        <img src="../assets/list.svg" alt="edit habit" />
+      <Link to="/edit-habit" className={activeScreen === 'edit-habit' ? 'active' : ''}>
+        <img src={listIcon} alt="edit habit" />
       </Link>
-      <Link
-        to="/progress-report"
-        className={activeScreen === "progress-report" ? "active" : ""}
-      >
-        <img src="../assets/progress.svg" alt="progress report" />
+      <Link to="/progress-report" className={activeScreen === 'progress-report' ? 'active' : ''}>
+        <img src={progressIcon} alt="progress report" />
       </Link>
-      <Link to="/" className={activeScreen === "main" ? "active" : ""}>
-        <img src="../assets/home.svg" alt="home" />
+      <Link to="/" className={activeScreen === 'main' ? 'active' : ''}>
+        <img src={homeIcon} alt="home" />
       </Link>
     </nav>
   );
