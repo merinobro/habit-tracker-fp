c9e03f66350bef93416cc3742b38f5cb75598791
khrystyna.dudko <khrystyna.dudko89@gmail.com>
2023-09-28 11:51:07 +0200

    settings screen functionality is added to the main screen

--- client/src/components/HabitCard.jsx
+++ client/src/components/HabitCard.jsx
@@ -1,58 +1,10 @@
-// import React, { useState, useEffect } from 'react';
-// import '../styles/HabitCard.css'; 
 
-// const HabitCard = ({ text: initialText, completed: initialCompleted, onUpdate }) => {
-//   // Initialize the state with the initialText and initialCompleted values
-//   const [text, setText] = useState(initialText || '');
-//   const [completed, setCompleted] = useState(initialCompleted || false);
 
-//   const handleTextChange = (e) => {
-//     setText(e.target.value);
-//   };
-
-//   const handleCheckboxChange = () => {
-//     setCompleted(!completed);
-//   };
-
-//   useEffect(() => {
-//     // Check if the state has changed before logging
-//     if (text !== initialText || completed !== initialCompleted) {
-//       onUpdate({ text, completed }); // Notify the parent component of the update
-//       console.log('Text state:', text);
-//       console.log('Completed state:', completed);
-//     }
-//   }, [text, completed, onUpdate, initialText, initialCompleted]);
-
-//   return (
-//     <div className="box">
-//       <div className="rectangle-eat-card">
-//         <div className="input-container">
-//           <input
-//             type="text"
-//             className="card-input"
-//             placeholder="Enter your habit"
-//             value={text}
-//             onChange={handleTextChange}
-//           />
-
-//           <input
-//             type="checkbox"
-//             className="card-checkbox"
-//             checked={completed}
-//             onChange={handleCheckboxChange}
-//           />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default HabitCard;
 
 // import React, { useState, useEffect } from 'react';
 // import '../styles/HabitCard.css';
 
-// const HabitCard = ({ text: initialText, completed: initialCompleted, onUpdate, onDelete }) => {
+// const HabitCard = ({ id, text: initialText, completed: initialCompleted, onUpdate, onDelete }) => {
 //   const [text, setText] = useState(initialText || '');
 //   const [completed, setCompleted] = useState(initialCompleted || false);
 //   const [isEditing, setIsEditing] = useState(false);
@@ -66,24 +18,22 @@
 //   };
 
 //   const handleDelete = () => {
-//     // Call the onDelete callback function to delete the card
-//     onDelete();
+//     onDelete(id);
 //   };
 
 //   useEffect(() => {
 //     if (text !== initialText || completed !== initialCompleted) {
-//       onUpdate({ text, completed });
-//       console.log('Text state:', text);
-//       console.log('Completed state:', completed);
+//       onUpdate({ id, text, completed });
 //     }
-//   }, [text, completed, onUpdate, initialText, initialCompleted]);
+//   }, [text, completed, onUpdate, initialText, initialCompleted, id]);
 
 //   return (
 //     <div className="box">
+//       <div className="input-container">
 //       <div className="rectangle-eat-card">
-//         <div className="input-container">
+        
 //           {isEditing ? (
-//             <>
+//             <div className="input-content">
 //               <input
 //                 type="text"
 //                 className="card-input"
@@ -97,7 +47,7 @@
 //                 onClick={handleDelete}
 //                 className="trash-icon"
 //               />
-//             </>
+//             </div>
 //           ) : (
 //             <>
 //               <span className="habit-text">{text}</span>
@@ -124,7 +74,6 @@
 
 // export default HabitCard;
 
-
 import React, { useState, useEffect } from 'react';
 import '../styles/HabitCard.css';
 
@@ -135,6 +84,10 @@ const HabitCard = ({ id, text: initialText, completed: initialCompleted, onUpdat
 
   const handleTextChange = (e) => {
     setText(e.target.value);
+  };
+
+  const toggleEditing = () => {
+    setIsEditing(!isEditing);
   };
 
   const handleCheckboxChange = () => {
@@ -154,8 +107,7 @@ const HabitCard = ({ id, text: initialText, completed: initialCompleted, onUpdat
   return (
     <div className="box">
       <div className="input-container">
-      <div className="rectangle-eat-card">
-        
+        <div className="rectangle-eat-card">
           {isEditing ? (
             <div className="input-content">
               <input
@@ -184,12 +136,20 @@ const HabitCard = ({ id, text: initialText, completed: initialCompleted, onUpdat
             </>
           )}
         </div>
-        <div className="card-options" onClick={() => setIsEditing(!isEditing)}>
-          <img
-            src={require('../assets/ellipsis.svg').default}
-            alt="Ellipsis Icon"
-            className="ellipsis-icon"
-          />
+        <div className="card-options" onClick={toggleEditing}>
+          {isEditing ? (
+           <img
+           src={require('../assets/save.svg').default}
+           alt="Checkbox Icon"
+           className="checkbox-icon"
+         />
+          ) : (
+            <img
+              src={require('../assets/ellipsis.svg').default}
+              alt="Ellipsis Icon"
+              className="ellipsis-icon"
+            />
+          )}
         </div>
       </div>
     </div>
@@ -197,3 +157,7 @@ const HabitCard = ({ id, text: initialText, completed: initialCompleted, onUpdat
 }
 
 export default HabitCard;
+
+
+
+
