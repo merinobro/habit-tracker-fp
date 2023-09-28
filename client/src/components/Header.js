393e52642ff5dee10111bd4f03f7c79dc8a04708
merinobro <116284900+merinobro@users.noreply.github.com>
2023-09-28 12:00:47 +0200

    header rewritten, maybe needs a CSS fix or two later, depending on how it looks on a Screen component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import '../styles/header.css'
 
-
-const Header = props => {
-  return (
-    <header >
-      <h1> Habit Tracker</h1>
-      
-    </header>
-  );
+const Header = ({ title }) => {
+    return (
+        <div className="header-container">
+            <h1 className="header-title">{title}</h1>
+            <hr className="header-divider" />
+        </div>
+    );
 };
 
 export default Header;
